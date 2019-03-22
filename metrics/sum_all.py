57a91b47a8f99dfe22841ffe36635d75314dbaaf
rpadmanabhan <raghus606@gmail.com>
2019-03-22 19:14:16 +0000

    Output detail logic for prep module as well

--- metrics/sum_all.py
+++ metrics/sum_all.py
@@ -7,15 +7,14 @@ def run(cfg):
     umi_filter = "umi_filter"
     duplex     = "duplex"
     prep       = "prep"
-    if cfg.outputDetail:
+    if cfg.outputDetail: # more details if needed
         prep       = "prep.detail"
         umi_filter = "umi_filter.detail"
         duplex     = "duplex.detail"
-
         
     # concatenate summary files
     with open(readSet + ".sum_all.summary.txt", "w") as OUT:
-        for fileType in ("prep", "align", umi_filter, "umi_frags", "sum.uniformity.primer", "umi_depths", duplex, "smCounter", "vcf_complex"):
+        for fileType in (prep, "align", umi_filter, "umi_frags", "sum.uniformity.primer", "umi_depths", duplex, "smCounter", "vcf_complex"):
             '''
             prep, align           -->  trimming metrics                 : core/prep.py, misc/process_ion.py (for ion reads)
             umi_filter            -->  primer finding metrics           : core/umi_filter.py
